32c2b84e922f651770bb83ff089302de557fa632
Zhengxu Chen <zhxchen17@outlook.com>
2021-08-02 00:56:34 -0700

    Updates

---
+++ component---src-pages-404-js-2ed777c5905582b5eb1c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_wordpress_blog=self.webpackChunkgatsby_starter_wordpress_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var s=a(7294),n=a(7198),r=a(6179);e.default=function(t){var e=t.data,a=t.location,l=e.site.siteMetadata.title;return s.createElement(n.Z,{location:a,title:l},s.createElement(r.Z,{title:"404: Not Found"}),s.createElement("h1",null,"404: Not Found"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2ed777c5905582b5eb1c.js.map
